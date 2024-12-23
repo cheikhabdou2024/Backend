3a91bb04012a752fed8c760b0b221aeeecbb48b7
cheikhabdou2024 <155577873+cheikhabdou2024@users.noreply.github.com>
2024-12-23 11:23:49 +0000

    Create video_models.js

---
+++ src/models/video_models.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const User = require('./User');
+
+const Video = sequelize.define('Video', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true,
+  },
+  url: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: User,
+      key: 'id',
+    },
+    allowNull: false,
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    defaultValue: DataTypes.NOW,
+  },
+});
+
+User.hasMany(Video, { foreignKey: 'userId' }); // Relation User -> Videos
+Video.belongsTo(User, { foreignKey: 'userId' }); // Relation Videos -> User
+
+module.exports = Video;
