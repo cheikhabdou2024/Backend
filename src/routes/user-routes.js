2d0e61850cee41f4b8fd7c36edd581e76bd92b09
cheikhabdou2024 <155577873+cheikhabdou2024@users.noreply.github.com>
2024-12-31 14:27:29 +0100

    Update user-routes.js

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getUser, updateUser } = require('../controllers/user-controller'); // Importer les fonctions du contrôleur
+const { getUser, updateUser , getUserWithDetails} = require('../controllers/user-controller'); // Importer les fonctions du contrôleur
 const { validateUserUpdate } = require('../utils/validator'); // Middleware de validation
 const router = express.Router();
 
@@ -8,6 +8,10 @@ router.get('/:id', getUser);
 
 // Route pour mettre à jour un utilisateur par ID
 router.put('/:id', validateUserUpdate, updateUser);
+
+
+// New route for detailed user profile
+router.get('/details/:id', getUserWithDetails);
 
 module.exports = router;
 
