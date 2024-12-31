3ae676718ce6d507d69ef1459ca4c02340641f48
cheikhabdou2024 <155577873+cheikhabdou2024@users.noreply.github.com>
2024-12-31 14:00:59 +0100

    Update like.js

--- src/models/like.js
+++ src/models/like.js
@@ -1,1 +1,37 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const Video = require('./Video');
+const User = require('./User');
+
+const Like = sequelize.define('Like', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    videoId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Video,
+            key: 'id',
+        },
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: User,
+            key: 'id',
+        },
+    },
+});
+
+Video.hasMany(Like, { foreignKey: 'videoId', as: 'likes' });
+Like.belongsTo(Video, { foreignKey: 'videoId', as: 'video' });
+
+User.hasMany(Like, { foreignKey: 'userId', as: 'userLikes' });
+Like.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+module.exports = Like;
 
